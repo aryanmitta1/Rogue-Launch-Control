c59536f067b49ad8c7b97d1046d3fa5e24aa7940
Safal Ghimire <safal_g@outlook.com>
2025-11-03 00:19:01 -0500

    Commented out some references to lat/long in transmitter_random.ino to reflect LCS not being responsible for GPS

--- transmitter_random.ino
+++ transmitter_random.ino
@@ -13,22 +13,22 @@ void loop() {
   int pressure = random(1, 10);
   int altitude = random(1, 10);
   int temperature = random(1, 10);
-  int latitude = random(1, 10);
-  int longitude = random(1, 10);
+  //int latitude = random(1, 10);
+  //int longitude = random(1, 10);
 
   data[0] = pressure;
   data[1] = altitude;
   data[2] = temperature;
-  data[3] = latitude;
-  data[4] = longitude;
+  //data[3] = latitude;
+  //data[4] = longitude;
 
-  // Construct and send message: <6,6,8,8,7>
+  // Construct and send message: <6,6,8>
   XBee.print('<');
   Serial.print('<');
-  for (int i = 0; i < 5; i++) {
+  for (int i = 0; i < 3; i++) {
     XBee.print(data[i]);
     Serial.print(data[i]);
-    if (i < 4) {
+    if (i < 2) {
       XBee.print(',');
       Serial.print(',');
     }
